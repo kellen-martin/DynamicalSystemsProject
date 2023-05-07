70a869c0c0d2b55141bdac6ce4448e95d2e9e762
Kellen Martin <kema0161@colorado.edu>
2023-05-06 21:59:02 -0600

    creates function to solve for accelerations

--- tools.py
+++ tools.py
@@ -43,11 +43,7 @@ def Naive3BP(r_0, v_0, masses, t_0, t_f, h):
         r_32 = -r_23
 
         # Calculate Accelerations
-        a_1 = -G*m_2*(r_12)/(distance(r_12)**3) - G*m_3*(r_13)/(distance(r_13)**3)
-        a_2 = -G*m_1*(r_21)/(distance(r_21)**3) - G*m_3*(r_23)/(distance(r_23)**3)
-        a_3 = -G*m_1*(r_31)/(distance(r_31)**3) - G*m_2*(r_32)/(distance(r_32)**3)
-        if i == 1:
-            print(a_3)
+        a_1, a_2, a_3 = get_acceleration(r_1[i-1,:], r_2[i-1,:], r_3[i-1,:], m_1, m_2, m_3, G)
 
         # Update Vectors
         v_1[i,:] = v_1[i-1,:] + h*a_1
@@ -89,8 +85,27 @@ def Symplectic_Leapfrog(r_0, v_0, masses, t_0, t_f, h):
 
     # Leapfrog Method
 
-    
+    # Step 1, get v_1/2
+
     return r_1, v_1, r_2, v_2, r_3, v_3
+
 def distance(r_ij):
     distance = np.sqrt( (r_ij[0])**2 + (r_ij[1])**2 + (r_ij[2])**2)
     return distance
+
+# Calculates acceleration for each body 
+def get_acceleration(r_1,r_2,r_3,m_1,m_2,m_3,G):
+# Relative Positions
+        r_12 = r_1 - r_2
+        r_13 = r_1 - r_3
+        r_23 = r_2 - r_3
+        r_21 = -r_12
+        r_31 = -r_13
+        r_32 = -r_23
+
+        # Calculate Accelerations
+        a_1 = -G*m_2*(r_12)/(distance(r_12)**3) - G*m_3*(r_13)/(distance(r_13)**3)
+        a_2 = -G*m_1*(r_21)/(distance(r_21)**3) - G*m_3*(r_23)/(distance(r_23)**3)
+        a_3 = -G*m_1*(r_31)/(distance(r_31)**3) - G*m_2*(r_32)/(distance(r_32)**3)
+    
+        return a_1, a_2, a_3
