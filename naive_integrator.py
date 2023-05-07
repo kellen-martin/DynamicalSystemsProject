483d64751a3eb108f8b33eaca6205541d3db4ccc
Kellen Martin <kema0161@colorado.edu>
2023-05-07 14:21:53 -0600

    PEFRL method

--- naive_integrator.py
+++ naive_integrator.py
@@ -6,6 +6,7 @@ plt.style.use('dark_background')
 from tools import *
 
 def main():
+    G = 6.6743*10**(-11)# [m^3/kg*s^2]
     # Sun-Earth-Moon IC
     r_0 = np.array([[0.0, 0.0, 0.0], [147.1*10**9, 0, 0], [147.1*10**9 + 384.4*10**6, 0, 0]])
     v_0 = np.array([[0.0, 0.0, 0.0], [0, 29.783*10**3, 0], [0, 29.783*10**3 + 1022, 0]])
@@ -16,33 +17,17 @@ def main():
     #v_0 = np.array([[7*10**3, -6*10**3, 0.0], [0, 9.783*10**3, 0], [0, 50*10**3 + 1022, 0]])
     #masses = np.array([1.989*10**30, 5.972*10**24, 7.342*10**22])
     t_0 = 0
-    t_f = 86400*365
-    h = 100
+    t_f = 86400*365*30
+    h = 86400
 
     #r_1,v_1,r_2,v_2,r_3,v_3 = Naive3BP(r_0, v_0, masses, t_0, t_f, h)
     #r_1,v_1,r_2,v_2,r_3,v_3 = Symplectic_Leapfrog(r_0, v_0, masses, t_0, t_f, h)
     r_1,v_1,r_2,v_2,r_3,v_3 = PEFRL_3BP(r_0, v_0, masses, t_0, t_f, h)
 
-
+    E = total_energy(r_1, v_1, r_2, v_2, r_3, v_3, masses, G)
     #print(r_3)
-    fig = plt.figure(figsize=(12,12))
-    ax = fig.add_subplot(111, projection='3d')
-    plt.gca().patch.set_facecolor('black')
-    ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
-    # plot the trajectory of objects
-    ax.plot(r_1[:,0],r_1[:,1],r_1[:,2],'y-')
-    ax.plot(r_2[:,0],r_2[:,1],r_2[:,2], 'b-')
-    ax.plot(r_3[:,0],r_3[:,1],r_3[:,2], 'r--')
-
-    # plot starting points
-    ax.plot(r_1[0,0],r_1[0,1],r_1[0,2],'y*')
-    ax.plot(r_2[0,0],r_2[0,1],r_2[0,2], 'b*')
-    ax.plot(r_3[0,0],r_3[0,1],r_3[0,2], 'r*')
-
-
-    # Mark Starting Point
-
-    plt.show()
+    plot_3BP(r_1, r_2, r_3)
+    plot_energy(E, t_0, t_f)
     return
 
 main()
--- tools.py
+++ tools.py
@@ -161,7 +161,7 @@ def PEFRL_3BP(r_0, v_0, masses, t_0, t_f, h):
          # Step 4 
          a1, a2, a3 = get_acceleration(r1_temp, r2_temp, r3_temp, m_1, m_2, m_3, G)
          v1_temp = v1_temp + lambda_*h*a1
-         v2_temp = v3_temp + lambda_*h*a2
+         v2_temp = v2_temp + lambda_*h*a2
          v3_temp = v3_temp + lambda_*h*a3
 
          # Step 5
@@ -228,3 +228,66 @@ def get_com(r_1,r_2,r_3,m_1,m_2,m_3):
      com = (r_1*m_1 + r_2*m_2 + r_3*m_3)/(m_1 + m_2 + m_3)
      return com
 
+# Calculates the total kinetic energy of the 3BP
+def kinetic_energy(v_1,v_2,v_3, m_1, m_2, m_3):
+     # KE = sum(1/2mv^2)
+     KE = .5*np.linalg.norm(v_1)*m_1 + .5*np.linalg.norm(v_2)*m_2 + .5*np.linalg.norm(v_3)*m_3
+     return KE
+
+def potnetial_energy(r_1, r_2, r_3, m_1, m_2, m_3, G):
+        r_12 = r_1 - r_2
+        r_13 = r_1 - r_3
+        r_23 = r_2 - r_3
+     
+        U_12 = -G*(m_1*m_2)/distance(r_12);
+        U_13 = -G*(m_1*m_3)/distance(r_13);
+        U_23 = -G*(m_2*m_3)/distance(r_23);
+
+        U = U_12 + U_13 + U_23;
+
+        return U
+
+def total_energy(r_1, v_1, r_2, v_2, r_3, v_3, masses, G):
+        E = np.zeros((len(r_1)))
+        m_1 = masses[0]
+        m_2 = masses[1]
+        m_3 = masses[2]
+
+        for i in range(0,len(r_1)):
+             KE = kinetic_energy(v_1[i,:],v_2[i,:],v_3[i,:], m_1, m_2, m_3)
+             U = potnetial_energy(r_1[i,:], r_2[i,:], r_3[i,:], m_1, m_2, m_3, G)
+             E[i] = KE + U
+
+        
+        return E
+
+def plot_3BP(r_1,r_2,r_3):
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(111, projection='3d')
+    plt.gca().patch.set_facecolor('black')
+    ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+    # plot the trajectory of objects
+    ax.plot(r_1[:,0],r_1[:,1],r_1[:,2],'y-')
+    ax.plot(r_2[:,0],r_2[:,1],r_2[:,2], 'b-')
+    ax.plot(r_3[:,0],r_3[:,1],r_3[:,2], 'r--')
+
+    # plot starting points
+    ax.plot(r_1[0,0],r_1[0,1],r_1[0,2],'y*')
+    ax.plot(r_2[0,0],r_2[0,1],r_2[0,2], 'b*')
+    ax.plot(r_3[0,0],r_3[0,1],r_3[0,2], 'r*')
+
+
+    # Mark Starting Point
+
+    plt.show()
+    return
+
+def plot_energy(E, t_0, t_f):
+     times = np.linspace(t_0, t_f, len(E))
+
+     plt.plot(times, E)
+     plt.ylabel("Energy [J]")
+     plt.xlabel("Time [s]")
+     plt.title("Total Energy vs Time")
+
+     plt.show()
